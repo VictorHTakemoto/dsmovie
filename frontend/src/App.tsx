0f2195c2201da8ae6067f690c5ee76b7910a1c52
Victor Takemoto <victor_takemoto@outlook.com>
2022-05-10 14:52:22 -0300

    Projct clean

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,20 @@
+import {ReactComponent as GithubIcon} from './assets/img/github.svg'
+
 function App() {
   return (
-    <h1>Página principal</h1>
+    <header>
+      <nav>
+        <div>
+          <h1>DSMovie</h1>
+          <a href="https://github.com/VictorHTakemoto">
+            <div>
+              <GithubIcon />
+              <p>/VictorTakemoto</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
   );
 }
 
