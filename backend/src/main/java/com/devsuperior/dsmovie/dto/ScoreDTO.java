56611fa35b12e0792abdc8b9b642b83453c51aea
Victor Takemoto <victor_takemoto@outlook.com>
2022-05-13 14:06:15 -0300

    Save score bug fixed

--- backend/src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
@@ -2,7 +2,7 @@ package com.devsuperior.dsmovie.dto;
 
 public class ScoreDTO {
 
-	private Long moveId;
+	private Long MovieId;
 	private String email;
 	private Double score;
 	
@@ -10,12 +10,12 @@ public class ScoreDTO {
 		
 	}
 
-	public Long getMoveId() {
-		return moveId;
+	public Long getMovieId() {
+		return MovieId;
 	}
 
-	public void setMoveId(Long moveId) {
-		this.moveId = moveId;
+	public void setMovieId(Long MovieId) {
+		this.MovieId = MovieId;
 	}
 
 	public String getEmail() {
